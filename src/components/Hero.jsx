e539af334b5f09bb5b6f7e4bea319c37c1523700
QAM <54406289+qamar62@users.noreply.github.com>
2024-11-06 16:10:26 +0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
           <h1 className="text-4xl sm:text-7xl font-bold text-gray-800 dark:text-white">Qamar Ibrahim</h1>
           <h2 className="text-4xl sm:text-7xl font-bold bg-gradient-to-r from-[#ff9966] to-[#ff5e62] text-transparent bg-clip-text">Full Stack Developer</h2>
           <p className="text-gray-600 dark:text-gray-400 py-4 max-w-[700px]">
-            I'm a full-stack developer specializing in building exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications.
+            I'm a full-stack developer specializing in building exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications with CI/CD and Ai Automation workflows .
           </p>
           <div className="flex space-x-4">
             <a href="#" className="text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-gradient-to-r hover:from-[#ff9966] hover:to-[#ff5e62] hover:border-transparent transition-all duration-300">
